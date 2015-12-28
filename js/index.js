bd02e2aaa4110e130fe6a8d290d19009572fde4b
lagendre@yahoo.com.tw <lagendre@yahoo.com.tw>
2015-12-28 21:40:30 +0800

    update music and background

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 $(function(){
 
+     var audio = new Audio('./remember_day.mp3');
+    
 //below android 3 not support 
 var ua = navigator.userAgent;
 if( ua.indexOf("Android") >= 0 )
@@ -13,17 +15,22 @@ if( ua.indexOf("Android") >= 0 )
 }
 
   if (!$('.envelope').hasClass('open')){
+      
     $('.envelope').click(function(){
+        
+        audio.play();
+        
       $(this).removeClass('new').addClass('open');
 	  
 	  	var refreshIntervalId =  setInterval(function() {
 		 if ( $("#wedding").turn('page')!= $("#wedding").turn('pages') ) {
 		    $("#wedding").turn('next');
-			
-			clearInterval(refreshIntervalId);
 		 }
+            clearInterval( refreshIntervalId );
+            
 	}, 3500);
-    });     	
+    });
+      
   }
 
 //receiver on cover    
@@ -71,10 +78,34 @@ if ( document.cookie.indexOf("visited") == -1 ) {
 
 	$("#wedding").turn({
 		width:  1024,
-		height: 748,  
+		height: 748,
+        elevation: 50,
+        gradients: true,
 		autoCenter: true
 	});    
     
+    
+    //love background 
+//thank for http://www.thelittletechie.com/2015/03/love-heart-animation-using-css3.html
+var love = setInterval(function() {
+    var r_num = Math.floor(Math.random() * 40) + 1;
+    var r_size = Math.floor(Math.random() * 65) + 10;
+    var r_left = Math.floor(Math.random() * 100) + 1;
+    var r_bg = Math.floor(Math.random() * 25) + 100;
+    var r_time = Math.floor(Math.random() * 5) + 5;
+
+    $('.bg_heart').append("<div class='heart' style='width:" + r_size + "px;height:" + r_size + "px;left:" + r_left + "%;background:rgba(255," + (r_bg - 25) + "," + r_bg + ",1);-webkit-animation:love " + r_time + "s ease;-moz-animation:love " + r_time + "s ease;-ms-animation:love " + r_time + "s ease;animation:love " + r_time + "s ease'></div>");
+
+    $('.bg_heart').append("<div class='heart' style='width:" + (r_size - 10) + "px;height:" + (r_size - 10) + "px;left:" + (r_left + r_num) + "%;background:rgba(255," + (r_bg - 25) + "," + (r_bg + 25) + ",1);-webkit-animation:love " + (r_time + 5) + "s ease;-moz-animation:love " + (r_time + 5) + "s ease;-ms-animation:love " + (r_time + 5) + "s ease;animation:love " + (r_time + 5) + "s ease'></div>");
+
+    $('.heart').each(function() {
+        var top = $(this).css("top").replace(/[^-\d\.]/g, '');
+        var width = $(this).css("width").replace(/[^-\d\.]/g, '');
+        if (top <= -100 || width >= 150) {
+            $(this).detach();
+        }
+    });
+}, 500);
 });
 
 
