54b65c4e039ee36dfaf5059853f22d241b6646db
lagendre@yahoo.com.tw <lagendre@yahoo.com.tw>
2015-12-28 22:41:56 +0800

    update to be vertical center

--- js/index.js
+++ js/index.js
@@ -16,9 +16,33 @@ if( ua.indexOf("Android") >= 0 )
 
   if (!$('.envelope').hasClass('open')){
       
+      //start to click function
     $('.envelope').click(function(){
         
         audio.play();
+                  
+    //love background 
+    //thank for http://www.thelittletechie.com/2015/03/love-heart-animation-using-css3.html
+    var love = setInterval(function() {
+        var r_num = Math.floor(Math.random() * 40) + 1;
+        var r_size = Math.floor(Math.random() * 65) + 10;
+        var r_left = Math.floor(Math.random() * 100) + 1;
+        var r_bg = Math.floor(Math.random() * 25) + 100;
+        var r_time = Math.floor(Math.random() * 5) + 5;
+
+        $('.bg_heart').append("<div class='heart' style='width:" + r_size + "px;height:" + r_size + "px;left:" + r_left + "%;background:rgba(255," + (r_bg - 25) + "," + r_bg + ",1);-webkit-animation:love " + r_time + "s ease;-moz-animation:love " + r_time + "s ease;-ms-animation:love " + r_time + "s ease;animation:love " + r_time + "s ease'></div>");
+
+        $('.bg_heart').append("<div class='heart' style='width:" + (r_size - 10) + "px;height:" + (r_size - 10) + "px;left:" + (r_left + r_num) + "%;background:rgba(255," + (r_bg - 25) + "," + (r_bg + 25) + ",1);-webkit-animation:love " + (r_time + 5) + "s ease;-moz-animation:love " + (r_time + 5) + "s ease;-ms-animation:love " + (r_time + 5) + "s ease;animation:love " + (r_time + 5) + "s ease'></div>");
+
+        $('.heart').each(function() {
+            var top = $(this).css("top").replace(/[^-\d\.]/g, '');
+            var width = $(this).css("width").replace(/[^-\d\.]/g, '');
+            if (top <= -100 || width >= 150) {
+                $(this).detach();
+            }
+        });
+    }, 400);
+    
         
       $(this).removeClass('new').addClass('open');
 	  
@@ -28,12 +52,12 @@ if( ua.indexOf("Android") >= 0 )
 		 }
             clearInterval( refreshIntervalId );
             
-	}, 3500);
+	}, 4000);
     });
       
   }
 
-//receiver on cover    
+//receiver name on cover    
     if ( window.location.href.indexOf('#') != -1  ){
 		var cName =decodeURIComponent(window.location.href.slice(window.location.href.indexOf('#') + 1));
 		 
@@ -53,96 +77,18 @@ if( ua.indexOf("Android") >= 0 )
 		$(".receiver").text( "○ ○ ○");
 	}
 
-$("#content-slider").lightSlider({
-                //loop:true,
-				auto: true,
-                keyPress:true,
-				item:1,
-				 speed:500
-        });
-	
 
-		
-// close it when closeLink is clicked
-$('a.closeLink').click( doOverlayClose );    
-
-   
-    
-    
-if ( document.cookie.indexOf("visited") == -1 ) {
-           // open tutorial mask
-            doOverlayOpen();
-    
-    document.cookie ="visited=yes;  expires=Fri, 31 Dec 9999 23:59:59 GMT";
-}
-
-	$("#wedding").turn({
+//turn.js 
+$("#wedding").turn({
 		width:  1024,
 		height: 748,
         elevation: 50,
         gradients: true,
 		autoCenter: true
-	});    
+});    
     
-    
-    //love background 
-//thank for http://www.thelittletechie.com/2015/03/love-heart-animation-using-css3.html
-var love = setInterval(function() {
-    var r_num = Math.floor(Math.random() * 40) + 1;
-    var r_size = Math.floor(Math.random() * 65) + 10;
-    var r_left = Math.floor(Math.random() * 100) + 1;
-    var r_bg = Math.floor(Math.random() * 25) + 100;
-    var r_time = Math.floor(Math.random() * 5) + 5;
 
-    $('.bg_heart').append("<div class='heart' style='width:" + r_size + "px;height:" + r_size + "px;left:" + r_left + "%;background:rgba(255," + (r_bg - 25) + "," + r_bg + ",1);-webkit-animation:love " + r_time + "s ease;-moz-animation:love " + r_time + "s ease;-ms-animation:love " + r_time + "s ease;animation:love " + r_time + "s ease'></div>");
-
-    $('.bg_heart').append("<div class='heart' style='width:" + (r_size - 10) + "px;height:" + (r_size - 10) + "px;left:" + (r_left + r_num) + "%;background:rgba(255," + (r_bg - 25) + "," + (r_bg + 25) + ",1);-webkit-animation:love " + (r_time + 5) + "s ease;-moz-animation:love " + (r_time + 5) + "s ease;-ms-animation:love " + (r_time + 5) + "s ease;animation:love " + (r_time + 5) + "s ease'></div>");
-
-    $('.heart').each(function() {
-        var top = $(this).css("top").replace(/[^-\d\.]/g, '');
-        var width = $(this).css("width").replace(/[^-\d\.]/g, '');
-        if (top <= -100 || width >= 150) {
-            $(this).detach();
-        }
-    });
-}, 500);
 });
 
 
-function doOverlayOpen() {
-	//set status to open
-	isOpen = true;
-	
-	//if box is not set to open then don't do anything
-	if( isOpen == false ) return;
-	// set the properties of the overlay box, the left and top positions
-	$('.overlayBox').css({
-		display:'block',
-		left:( $(window).width() - $('.overlayBox').width() )/2,
-		//top:( $(window).height() - $('.overlayBox').height() )/2 -80,
-		position:'absolute'
-	});
-	// set the window background for the overlay. i.e the body becomes darker
-	$('.bgCover').css({
-		display:'block',
-		width: $(window).width(),
-		height:$(window).height(),
-	});    
-    
-    
-	$('.bgCover').css({opacity:0}).animate( {opacity:0.5, backgroundColor:'#000'} );
-	// dont follow the link : so return false.
-	//return false;
-}
-function doOverlayClose() {
-
-	//set status to closed
-	isOpen = false;
-	$('.overlayBox').css( 'display', 'none' );
-	// now animate the background to fade out to opacity 0
-	// and then hide it after the animation is complete.
-	$('.bgCover').animate( {opacity:0}, null, null, function() { $(this).hide(); } );
-}
-// if window is resized then reposition the overlay box
-//$(window).bind('resize', showOverlayBox);
 
